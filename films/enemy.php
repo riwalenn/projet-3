35cd8e8d05344c6b499744dca9d0ac25ab52fb3c
Riwalenn <riwalenn@gmail.com>
2017-04-10 15:55:39 +0200

    modification css pour slider

---
+++ films/enemy.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <title>Festival : les films plein air !</title>
+  <meta name="description" content="Festival des films plein air du 5 au 8 août 2017 à partir de 18h. Entrée gratuite, rendez-vous au parc Monceau à Paris !" />
+  <meta name="keywords" content="films, plein air, festival, parc monceau, 5 au 8 aout, 18h-minuit, gratuit, Paris">
+  <meta name="author" content="Riwalenn Bas">
+	<link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="http://films-plein-air/css/bootstrap.min.css">
+	<link rel="stylesheet" href="http://films-plein-air/css/style.css">
+	<script src="https://use.fontawesome.com/a2fc004260.js"></script>
+</head>
+<body class="festival">
+<!-- ---------------------------------------------- 
+	                   HEADER
+	 ---------------------------------------------- -->
+	<?php 
+				include '../header.html'; 
+	?>
+
+<!-- ---------------------------------------------- 
+	            FILM (détail + formulaire)
+	 ---------------------------------------------- -->
+	<div class="row">
+		<div class="col-md-7">
+			<div class="row">
+				<div class="col-md-4">
+				<div class="img_g"><img src="../images/vignettes-details/enemy.jpg" alt="Film Enemy" /></div>
+				</div>
+				<div class="col-md-3"><span>Date de sortie</span>27 août 2014 (1h30min). De Denis Villeneuve. Avec Jake Gyllenhaal,
+				Mélanie Laurent, Sarah Gadon...<br />
+				<span>Genre</span> Thriller<br />
+				Nationalités Canadien, Espagnol.</div>
+				<div class="col-md-7">...3...</div>
+			</div>
+		</div>
+		<div class="col-md-5">...5...</div>
+	</div>
+
+<!-- ---------------------------------------------- 
+	                   FOOTER
+	 ---------------------------------------------- -->
+	 <?php 
+				include '../footer.html'; 
+	?>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<script src="http://films-plein-air/js/bootstrap.min.js"></script>
+</body>
+</html>
